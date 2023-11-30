73adab90a82a8ddea74a86e76e30224cd051d6fd
balbo97 <balbosauro1@gmail.com>
2023-11-30 23:23:38 +0100

    BONUS completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// CREO UNA VARIABILE A CUI ASSOCIO UN ELEMENTO NELL HTML 
-const button_play = document.getElementById('play');
 
 // CREO UNA FUNZIONE CHE MI GENERI NUMERI CASUALI DA 1 A X(dove x è il numero delle celle presenti nel livello) 
 function generate_numbers(x){
@@ -9,26 +7,26 @@ function generate_numbers(x){
 
 // CREO UNA FUNZIONE CHE MI GENERI UN ARRAY DI BOMBS 
 function generate_bombs(b, x){
-//  dove b & x sono corrispettivamente il numero di bombe ed x la quantità di celle 
-
+    //  dove b & x sono corrispettivamente il numero di bombe ed x la quantità di celle 
+    
     // DEFINISCO UN ARRAY VUOTO 
     let array = [];
     
     // CREO UN CICLO FOR CHE SI RIPETA TANTE VOLTE QUANTE SONO LE BOMBE  
     for(let i=0; i<b; i++){
-
+        
         // MI CREO UNA VARIABILE CHE ABBIA COME VALORE LA FUNZIONE generate_bombs 
-       let random_number = generate_numbers(x)
-    
-    //    CREO UN CICLO WHILE PER GENERARE NUMERI TUTTI DIVERSI DA LORO 
-    //    finche l'array include numeri random allora continua a generare 
-       while(array.includes(random_number)){
-        random_number = generate_numbers(x)
-       }
-    //    sennò me li pusha nell'array 
-       array.push(random_number)
+        let random_number = generate_numbers(x)
+        
+        //    CREO UN CICLO WHILE PER GENERARE NUMERI TUTTI DIVERSI DA LORO 
+        //    finche l'array include numeri random allora continua a generare 
+        while(array.includes(random_number)){
+            random_number = generate_numbers(x)
+        }
+        //    sennò me li pusha nell'array 
+        array.push(random_number)
     }
-
+    
     return array
 }
 
@@ -56,12 +54,18 @@ function createGrid(x, y){
     return cell;
 }
 
+// RECUPERO IL BUTTON CREANDO UNA VARIABILE 
+const button_play = document.getElementById('play');
+
 // RECUPERO LA GRIGLIA CREANDO UNA VARIABILE 
 let grid = document.getElementById('grid');
 
 //FACCIO IN MODO CHE QUANDO CLICCO SUL BUTTON NEL HTML LA MIA FUNZIONE VENGA MESSA IN ATTO 
-button_play.addEventListener('click', function(){
-    
+button_play.addEventListener('click', function(){ 
+
+    // IMPOSTO LA MIA GRIGLIA VUOTA PRIMA DEL CICLO FOR COSI' QUANDO VADO A CAMBIARE IL LIVELLO DI DIFFICOLTA ED A CLICCARE NUOVAMENTE IL BUTTON PLAY E' VUOTA E NON SI AGGIUNGIE A QUELLA VECCHIA 
+    grid.innerText = ''
+
     //  LIVELLO DI DIFFICOLTA' 
     let level = document.getElementById('difficulty').value 
     
@@ -84,23 +88,60 @@ button_play.addEventListener('click', function(){
         x = 0
         y = 0
     }
+
+    //UNA VARIABILE CHE CORRISPONDE AL NUMERO DI BOMBE 
+    let number_bombs = 16;
+
+    // DEFINISCO UNA COSTANTE CON I NUMERI DELLE BOMBE 
+    const bombs = generate_bombs(number_bombs, x)
     
-    // IMPOSTO LA MIA GRIGLIA VUOTA PRIMA DEL CICLO FOR COSI' QUANDO VADO A CAMBIARE IL LIVELLO DI DIFFICOLTA ED A CLICCARE NUOVAMENTE IL BUTTON PLAY E' VUOTA E NON SI AGGIUNGIE A QUELLA VECCHIA 
-    grid.innerText = ''
+    // CREO UNA VARIABILE POINTS 
+    let points = 0;
+
+    let game_over = false;
+    
+    
     
     //CREO UN CICLO FOR
     for (let i=0; i<x; i++){
         
         // CREO LO SQUARE 
         let square = createGrid(i+1, y);
+
         
         // APPENDO LO SQUARE ALLA GRID 
-        grid.appendChild(square)
+        grid.appendChild(square);
         
         // QUANDO L'UTENTE CLICCA SULLO SQUARE QUELLO CAMBIA COLORE 
         square.addEventListener('click', function(){
-            this.classList.toggle('clicked')
-            console.log(i+1)
+
+            if(!game_over){
+
+                if(!bombs.includes(i)){
+                    this.classList.add('clicked')
+                    points ++
+                    document.getElementById('points').innerText = `IL TUO PUNTEGGIO E': ${points}`
+                }
+                else{
+                    
+                    this.classList.add('bomb-clicked')
+                    
+                    game_over = true 
+    
+                    function delete_grid(){
+
+                        document.getElementById('points').innerText = ''
+                       return grid.innerHTML = `<h1> HAI PERSO! <br>IL TUO PUNTEGGIO E': ${points}`
+    
+                    }
+                    setTimeout(delete_grid, 500)
+                }
+            }
+            
+
+            
+            
+                
         })
     } 
     
