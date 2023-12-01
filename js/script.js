56dad3067be53966ba50f355d00def862f5e08e4
balbo97 <balbosauro1@gmail.com>
2023-12-01 21:55:26 +0100

    js when bomb clicked they all go red

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // CREO UNA FUNZIONE CHE MI GENERI NUMERI CASUALI DA 1 A X(dove x è il numero delle celle presenti nel livello) 
 function generate_numbers(x){
     // dove x, in questo caso, è il numero di celle e quindi di valore massimo che questi numeri potranno avere
-    return Math.floor(Math.random()*x +1)
+    return Math.floor(Math.random()*x)+1;
 }
 
 // CREO UNA FUNZIONE CHE MI GENERI UN ARRAY DI BOMBS 
@@ -94,52 +94,64 @@ button_play.addEventListener('click', function(){
 
     // DEFINISCO UNA COSTANTE CON I NUMERI DELLE BOMBE 
     const bombs = generate_bombs(number_bombs, x)
+    console.log(bombs)
     
     // CREO UNA VARIABILE POINTS 
     let points = 0;
 
+    
+    
+    // CREO UNA FLAG GAME OVER 
     let game_over = false;
-    
-    
     
     //CREO UN CICLO FOR
     for (let i=0; i<x; i++){
         
         // CREO LO SQUARE 
         let square = createGrid(i+1, y);
+       
 
         
         // APPENDO LO SQUARE ALLA GRID 
         grid.appendChild(square);
+
         
         // QUANDO L'UTENTE CLICCA SULLO SQUARE QUELLO CAMBIA COLORE 
         square.addEventListener('click', function(){
 
             if(!game_over){
 
-                if(!bombs.includes(i)){
-                    this.classList.add('clicked')
-                    points ++
-                    document.getElementById('points').innerText = `IL TUO PUNTEGGIO E': ${points}`
+                if(!bombs.includes(i+1)){
+                    if(!this.classList.contains('clicked')){
+                        
+                        this.classList.add('clicked')
+                        points ++
+                        document.getElementById('points').innerText = `IL TUO PUNTEGGIO E': ${points}`
+                    }
                 }
                 else{
+
+                   let cell = document.getElementById('grid').children;
+                   
+                   
+
+                    for(i=0; i<bombs.length; i++){
+
+                        cell[bombs[i]-1].classList.add('bomb-clicked')
+                    }
                     
                     this.classList.add('bomb-clicked')
+                    setTimeout(function(){
+                        
+                        document.getElementById('points').innerText = ''
+                        return grid.innerHTML = `<h1> HAI PERSO! <br>IL TUO PUNTEGGIO E': ${points}`
+                        
+                    }, 2000)
                     
                     game_over = true 
-    
-                    function delete_grid(){
-
-                        document.getElementById('points').innerText = ''
-                       return grid.innerHTML = `<h1> HAI PERSO! <br>IL TUO PUNTEGGIO E': ${points}`
-    
-                    }
-                    setTimeout(delete_grid, 500)
                 }
+                
             }
-            
-
-            
             
                 
         })
